dc36b4bae9daf4d7b7efb74ba1b122960c26db8f
DaniVSainz <danivsainz@gmail.com>
2017-05-01 09:19:49 -0400

    Gen devise user // db create // migrate

---
+++ devise-auth-token/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+
+
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :trackable,  
+         :validatable, 
+         :omniauthable
+  include DeviseTokenAuth::Concerns::User
+end
