5b92d24844481c64b6e206405302ea61e9e8c6a2
DaniVSainz <danivsainz@gmail.com>
2017-05-01 13:21:57 -0400

    Modal popups now working

--- angular-front-end/src/app/toolbar/toolbar.component.ts
+++ angular-front-end/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {AuthDialogComponent} from "../auth-dialog/auth-dialog.component";
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  @ViewChild('authDialog') authDialog: AuthDialogComponent;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  presentAuthDialog(mode?: 'login'| 'register'){
+    this.authDialog.openDialog(mode);
   }
 
 }
