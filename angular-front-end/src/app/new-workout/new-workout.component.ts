d70bdf2f006ff8815ad620f2c6e7afaefdf7703c
DaniVSainz <danivsainz@gmail.com>
2017-05-04 17:34:44 -0400

    Now passing user id from controller beefore form submission

--- angular-front-end/src/app/new-workout/new-workout.component.ts
+++ angular-front-end/src/app/new-workout/new-workout.component.ts
@@ -2,15 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { WorkoutService } from '../services/workout.service';
 import { Observable } from 'rxjs/Rx';
 import { Workout } from '../workout/workout';
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
+import {Angular2TokenService} from "angular2-token";
+
 @Component({
   selector: 'app-new-workout',
   templateUrl: './new-workout.component.html',
-  styleUrls: ['./new-workout.component.sass']
+  styleUrls: ['./new-workout.component.sass'],
+  providers: [WorkoutService]
 })
 export class NewWorkoutComponent implements OnInit {
   workout = new Workout;
+
   submitted: boolean = false;
-  constructor(
+
+  constructor(public authTokenService:Angular2TokenService,
+    protected authService:AuthService,
+    private router:Router,
     public workoutService: WorkoutService
   ) {}
 
@@ -20,6 +29,7 @@ export class NewWorkoutComponent implements OnInit {
   d = new Date();
 
   createWorkout(workout){
+    workout.user_id = this.authTokenService.currentUserData.id
     this.submitted = true;
     this.workoutService.createWorkout(workout)
         .subscribe(
