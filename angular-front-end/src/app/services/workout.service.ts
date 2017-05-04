78918588d7553d839bfef89ba083824738201218
DaniVSainz <danivsainz@gmail.com>
2017-05-04 09:08:11 -0400

    change address for workouts

--- angular-front-end/src/app/services/workout.service.ts
+++ angular-front-end/src/app/services/workout.service.ts
@@ -6,7 +6,7 @@ import { Workout } from '../workout/workout';
 
 @Injectable()
 export class WorkoutService {
-  private workoutsUrl = 'http://localhost:3000/workouts';
+  private workoutsUrl = 'https://lift-tracker--api.herokuapp.com/workouts';
 
   constructor(private http:Http){}
 
