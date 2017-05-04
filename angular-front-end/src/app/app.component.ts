c58c3d03e0d424eb889ab224477167daf27160fd
DaniVSainz <danivsainz@gmail.com>
2017-05-04 15:43:59 -0400

    small changees

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -11,19 +11,5 @@ export class AppComponent {
   title = 'app works!';
   constructor(private authToken: Angular2TokenService){
     this.authToken.init(environment.token_auth_config);
-
-    this.authToken.signIn({email: "", password: ""}).subscribe(
-
-        res => {
-
-          console.log('auth response:', res);
-          console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-          console.log('auth response body:', res.json()); //log the response body to show the user
-        },
-
-        err => {
-          console.error('auth error:', err);
-        }
-    )
   }
 }
--- angular-front-end/src/app/toolbar/toolbar.component.ts
+++ angular-front-end/src/app/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {AuthDialogComponent} from "../auth-dialog/auth-dialog.component";
 import {AuthService} from "../services/auth.service";
 import {Router} from "@angular/router";
+import {Angular2TokenService} from "angular2-token";
 
 @Component({
   selector: 'app-toolbar',
@@ -13,7 +14,7 @@ export class ToolbarComponent implements OnInit {
   @ViewChild('authDialog') authDialog: AuthDialogComponent;
 
 
-  constructor(public authService:AuthService, private router:Router) { }
+  constructor(public authService:AuthService, private router:Router,public authTokenService:Angular2TokenService) { }
 
   ngOnInit(){}
 
