46387dc55711dd72a710736993f2983541bcbfe0
DaniVSainz <danivsainz@gmail.com>
2017-05-06 12:32:43 -0400

    Work for new exercise form

---
+++ angular-front-end/src/app/workout/show-workout/exercise.ts
@@ -0,0 +1,21 @@
+export class Exercise {
+  name: string;
+  id: number;
+  workout_id: number;
+  set1: number;
+  weight1: number;
+  set2: number;
+  weight2: number;
+  set3: number;
+  weight3: number;
+  set4: number;
+  weight4: number;
+  set5: number;
+  weight5: number;
+  set6: number;
+  weight6: number;
+  set7: number;
+  weight7: number;
+}
+
+
