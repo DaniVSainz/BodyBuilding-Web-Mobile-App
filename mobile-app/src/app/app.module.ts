5c370dc5aab27e536c395dd476fd9c78713c5d1d
DaniVSainz <danivsainz@gmail.com>
2017-06-09 16:07:38 -0400

    Starter mobile app

---
+++ mobile-app/src/app/app.module.ts
@@ -0,0 +1,120 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule, Http } from '@angular/http';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage, IonicStorageModule } from '@ionic/storage';
+
+import { MyApp } from './app.component';
+
+import { CardsPage } from '../pages/cards/cards';
+import { ContentPage } from '../pages/content/content';
+import { ItemCreatePage } from '../pages/item-create/item-create';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { ListMasterPage } from '../pages/list-master/list-master';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { MenuPage } from '../pages/menu/menu';
+import { SearchPage } from '../pages/search/search';
+import { SettingsPage } from '../pages/settings/settings';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { WelcomePage } from '../pages/welcome/welcome';
+
+import { Api } from '../providers/api';
+import { Items } from '../mocks/providers/items';
+import { Settings } from '../providers/settings';
+import { User } from '../providers/user';
+
+import { Camera } from '@ionic-native/camera';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// The translate loader needs to know where to load i18n files
+// in Ionic's static asset pipeline.
+export function HttpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function provideSettings(storage: Storage) {
+  /**
+   * The Settings provider takes a set of default settings for your app.
+   *
+   * You can add new settings options at any time. Once the settings are saved,
+   * these values will not overwrite the saved values (this can be done manually if desired).
+   */
+  return new Settings(storage, {
+    option1: true,
+    option2: 'Ionitron J. Framework',
+    option3: '3',
+    option4: 'Hello'
+  });
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    CardsPage,
+    ContentPage,
+    ItemCreatePage,
+    ItemDetailPage,
+    ListMasterPage,
+    LoginPage,
+    MapPage,
+    MenuPage,
+    SearchPage,
+    SettingsPage,
+    SignupPage,
+    TabsPage,
+    TutorialPage,
+    WelcomePage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    }),
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    CardsPage,
+    ContentPage,
+    ItemCreatePage,
+    ItemDetailPage,
+    ListMasterPage,
+    LoginPage,
+    MapPage,
+    MenuPage,
+    SearchPage,
+    SettingsPage,
+    SignupPage,
+    TabsPage,
+    TutorialPage,
+    WelcomePage
+  ],
+  providers: [
+    Api,
+    Items,
+    User,
+    Camera,
+    GoogleMaps,
+    SplashScreen,
+    StatusBar,
+    { provide: Settings, useFactory: provideSettings, deps: [Storage] },
+    // Keep this to enable Ionic's runtime error handling during development
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ mobile-app/src/pages/search/search.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailPage } from '../item-detail/item-detail';
+
+import { Item } from '../../models/item';
+
+import { Items } from '../../providers/providers';
+
+
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html'
+})
+export class SearchPage {
+  
+  currentItems: any = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+
+  /**
+   * Perform a service for the proper items.
+   */
+  getItems(ev) {
+    let val = ev.target.value;
+    if (!val || !val.trim()) {
+      this.currentItems = [];
+      return;
+    }
+    this.currentItems = this.items.query({
+      name: val
+    });
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+
+}
