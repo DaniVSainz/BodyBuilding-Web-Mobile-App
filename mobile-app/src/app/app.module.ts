a31c4ffce1f536904ef4bf188f74b28ba57e12c7
DaniVSainz <danivsainz@gmail.com>
2017-06-12 15:30:45 -0400

    Create provider import ang2tokenserv

--- mobile-app/src/app/app.module.ts
+++ mobile-app/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+import { Angular2TokenService } from 'angular2-token';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthServiceProvider,
+    Angular2TokenService
   ]
 })
 export class AppModule {}
