177b77d52cc557ae14e5cb2df4df1df9e2438ac9
DaniVSainz <danivsainz@gmail.com>
2017-06-13 11:20:45 -0400

    Redirect now functions correctly

--- mobile-app/src/pages/contact/contact.ts
+++ mobile-app/src/pages/contact/contact.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AuthGuard} from '../../guards/auth.guard';
+import { HomePage } from '../../pages/home/home';
+
+
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
+  providers: [AuthGuard,HomePage]
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public authGuard: AuthGuard) {
 
   }
 
+   ionViewCanEnter() {
+    this.authGuard.canActivate();
+    if(this.authGuard.canActivate()){
+    }else{
+      this.navCtrl.push(HomePage);
+    }
+  }
+
+  ionViewDidEnter(){
+    this.authGuard.printLoginStatus();
+  }
 }
