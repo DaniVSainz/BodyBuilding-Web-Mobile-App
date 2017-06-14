5fded4ca74c456c938387406d1060d55e6f7e8a2
DaniVSainz <danivsainz@gmail.com>
2017-06-14 12:16:16 -0400

    more work in mobileApp

--- mobile-app/src/app/app.component.ts
+++ mobile-app/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import {Angular2TokenService} from "angular2-token";
 import {environment} from "../enviroments/enviroment";
 import {HomePage} from '../pages/home/home';
+import {ViewChild} from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+
+import{WorkoutPage} from '../pages/workout/workout';
 
 
 
@@ -14,6 +20,7 @@ import {HomePage} from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  @ViewChild('mycontent') nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private authToken: Angular2TokenService) {
     this.authToken.init(environment.token_auth_config);
@@ -23,6 +30,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  goToPage(){
+    this.nav.push(WorkoutPage);
   }
 }
 
--- mobile-app/src/pages/home/home.ts
+++ mobile-app/src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service/auth-service';
 import {Angular2TokenService} from "angular2-token";
+import {LoginPage} from '../login/login';
+import {WorkoutPage} from '../workout/workout';
 
 
 @Component({
@@ -10,8 +12,20 @@ import {Angular2TokenService} from "angular2-token";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,private authService: AuthService,public authTokenService:Angular2TokenService) {
+  // private workoutPage;
+  // private loginPage;
+  // private rootPage;
 
+
+  constructor(public navCtrl: NavController,private authService: AuthService,public authTokenService:Angular2TokenService) {
+    // this.rootPage = LoginPage;
+    // this.workoutPage = WorkoutPage;
+    // this.loginPage = LoginPage;
   }
+
+
+  // openPage(p) {
+  //   this.rootPage = p;
+  // }
 
 }
