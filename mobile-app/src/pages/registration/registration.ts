5ef645275c9b5db490eda40e7c6423811fa6d23a
DaniVSainz <danivsainz@gmail.com>
2017-06-25 17:15:44 -0400

    Form works,i can now die in peace

--- mobile-app/src/pages/registration/registration.ts
+++ mobile-app/src/pages/registration/registration.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {FormBuilder,FormGroup,Validators,AbstractControl} from '@angular/forms';
-
+import {AuthService} from '../../providers/auth-service/auth-service';
+import {HomePage} from '../home/home';
 
 @IonicPage()
 @Component({
@@ -14,15 +15,18 @@ export class RegistrationPage {
   registration:any;
   email: string;
   password: string;
-  passwordMatch: '';
+  passwordConfirmation: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private formBuilder:FormBuilder)
+  @Output() onFormResult = new EventEmitter<any>();
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private formBuilder:FormBuilder,private authService:AuthService)
     {
       this.rForm=formBuilder.group({
         'email': [null, Validators.compose([Validators.required,Validators.email])],
         'password': [null, Validators.compose([Validators.required,Validators.minLength(8)])],
-        'confirmPassword':['',Validators.compose([Validators.minLength(8),Validators.required])]
-      }, {validator: matchingPasswords('password', 'confirmPassword')});
+        'passwordConfirmation':['',Validators.compose([Validators.minLength(8),Validators.required])]
+      }, {validator: matchingPasswords('password', 'passwordConfirmation')});
 
         function matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
         return (group: FormGroup): {[key: string]: any} => {
@@ -42,10 +46,26 @@ export class RegistrationPage {
 
 
   createRegistration(registration){
-    this.email = registration.email;
-    this.password = registration.password;
+    // this.email = registration.email;
+    // this.password = registration.password;
+    // this.passwordConfirmation = registration.passwordConfirmation
     console.log(registration);
-    this.rForm.reset();
+    this.authService.registerUser(registration).subscribe(
+        (res) => {
+
+          if (res.status == 200){
+            this.onFormResult.emit({signedUp: true, res})
+            this.rForm.reset();
+            this.navCtrl.pop
+            this.navCtrl.push(HomePage);
+          }
+
+        },
+        (err) => {
+          console.log(err.json())
+          this.onFormResult.emit({signedUp: false, err})
+        }
+    )
   }
 
 }
