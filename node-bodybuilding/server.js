4592c719cc8ba3df8afb01f4e285b2bb48019979
danivsainz <danivsainz@gmail.com>
2018-01-18 20:45:38 -0500

    Delete old proj, keep restructured one

--- node-bodybuilding/server.js
+++ node-bodybuilding/server.js
@@ -40,7 +40,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'angular-src/dist')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -58,9 +58,9 @@ app.get('/', (req, res) => {
   res.send('invaild endpoint');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 // Start Server
 // app.listen(port, () => {
